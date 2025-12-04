75710a875be980ef493e0325eb2435738329459f
titdevang <devang@treeshainfotech.com>
2025-12-04 18:25:54 +0530

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ import { CategoryListCacheProvider } from "./context/CategoryListCacheContext";
 import AuthProviderWrapper from "./context/AuthProviderWrapper";
 import LoginModal from "./components/ui/modal/LoginModal";
 import { LocationHierarchyProvider } from "./context/LocationHierarchyContext";
-import {Analytics} from "@vercel/analytics/vue";
+import {Analytics} from "@vercel/analytics/next";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
